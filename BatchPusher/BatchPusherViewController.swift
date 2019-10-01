c9ae7395e4f3b70df63187d1fd5c496079243afe
Peter <peter@swag.live>
2019-10-01 19:06:03 +0800

    modify test code

--- BatchPusher/BatchPusherViewController.swift
+++ BatchPusher/BatchPusherViewController.swift
@@ -108,50 +108,14 @@ class BatchPusherViewController: UIViewController, PusherDelegate {
 }
 
 
-//class AuthRequestBuilder: AuthRequestBuilderProtocol {
-//    func requestFor(socketID: String, channelName: String) -> URLRequest? {
-//        var request = URLRequest(url: URL(string: "http://localhost:9292/pusher/auth")!)
-//        request.httpMethod = "POST"
-//        request.httpBody = "socket_id=\(socketID)&channel_name=\(channelName)".data(using: String.Encoding.utf8)
-//        return request
-//    }
-//}
 let deviceID = UUID().uuidString.lowercased()
 
 class AuthRequestBuilder: AuthRequestBuilderProtocol {
 
-//    func requestFor(socketID: String, channelName: String) -> URLRequest? {
-//        let urlString = "https://api.v2.swag.live/pusher/authenticate"
-//
-//        var request = URLRequest(url: URL(string: urlString)!)
-//        request.httpMethod = "POST"
-//        request.httpBody = "socket_id=\(socketID)&channel_name=\(channelName)".data(using: String.Encoding.utf8)
-//
-//        request.addValue(deviceID, forHTTPHeaderField: "'X-Client-ID")
-//
-//        return request
-//    }
-//    func requestFor(socketID: String, channelName: String) -> URLRequest? {
-//        let urlString = "https://api.v2.swag.live/pusher/batch-authenticate"
-//
-//        var request = URLRequest(url: URL(string: urlString)!)
-//        request.httpMethod = "POST"
-//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-//        request.addValue(deviceID, forHTTPHeaderField: "X-Client-ID")
-//
-//        let parameters: [String : Any] = ["socket_id": socketID,
-//                                          "channels": [channelName]]
-//        let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options:[])
-//        request.httpBody = httpBody
-//
-//        return request
-//    }
 
     func requestFor(socketID: String, channels: [PusherChannel]) -> URLRequest? {
         let urlString = "https://api.v2.swag.live/pusher/batch-authenticate"
 
-//        var request = PusherConnection.requestForAuthValue(from: urlString, socketId: socketID, channels: channels)
-        //        request.addValue(token, forHTTPHeaderField: "Authorization")
         var request = URLRequest(url: URL(string: urlString)!)
 
         request.httpMethod = "POST"
