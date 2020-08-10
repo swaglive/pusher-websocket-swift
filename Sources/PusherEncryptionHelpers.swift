3ecb620cddeee9d5c4faa41917413e7a65273f74
finnhe <finn.he@swag.live>
2020-08-10 14:53:33 +0800

    fix compile failed

--- Sources/PusherEncryptionHelpers.swift
+++ Sources/PusherEncryptionHelpers.swift
@@ -7,12 +7,14 @@ struct PusherEncryptionHelpers {
     }
 
     public static func isEncryptedChannel(channelName: String?) -> Bool {
-        return channelName?.starts(with: "private-enc-") ||
-            channelName?.starts(with: "presence-enc-") ||
-            channelName?.starts(with: "private-encrypted-") ?? false
+        guard let channelName = channelName else { return false }
+        return channelName.starts(with: "private-enc-") ||
+            channelName.starts(with: "presence-enc-") ||
+            channelName.starts(with: "private-encrypted-")
     }
 
     public static func isPusherSystemEvent(eventName: String) -> Bool {
         return eventName.starts(with: "pusher:") || eventName.starts(with: "pusher_internal:")
     }
+
 }
